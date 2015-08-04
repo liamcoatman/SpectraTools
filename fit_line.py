ec875e5e0abe605cf9d22a6f941d1d4b599ed8fd
Liam Coatman <lc585@ast.cam.ac.uk>
2015-08-04 17:16:24 +0100

    added empirical calculation of FWHM for multi-component models. tried to do same for sigma but I got confused

--- fit_line.py
+++ fit_line.py
@@ -17,6 +17,7 @@ from lmfit import minimize, Parameters, fit_report
 import numpy.ma as ma
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import integrate, optimize
 
 def resid(p,x,model,data=None,sigma=None):
 
@@ -313,6 +314,16 @@ def fit_line(wav,
                    args=(np.asarray(vdat), mod, ydat, yerr),
                    method ='nelder')
 
+    # Calculate FWHM of distribution
+    # Check for single gaussian / lorentzian that this agrees with the analytic
+    # result
+
+    integrand = lambda x: mod.eval(params=pars, x=np.array(x))
+    func_center = optimize.fmin(lambda x: -integrand(x) , 0)[0]
+    half_max = mod.eval(params=pars, x=func_center) / 2.0
+    root1 = optimize.brentq(lambda x: integrand(x) - half_max, -5.0e4, 0.0)
+    root2 = optimize.brentq(lambda x: integrand(x) - half_max, 0.0, 5.0e4)
+    print 'FWHM: {}'.format(root2 - root1)
 
     if verbose:
         print fit_report(pars)
@@ -337,7 +348,6 @@ def fit_line(wav,
                  plot_region=plot_region,
                  plot_title=plot_title)
 
-
 #    return pars, mod, x_data, dw, y_data_cr, y_sigma
     return None
 
@@ -345,11 +355,11 @@ def fit_line(wav,
 
 if __name__ == '__main__':
 
-    from get_spectra import get_boss_dr12_spec
+    from get_spectra import get_liris_spec
     import matplotlib.pyplot as plt
 
-#    fname = '/data/lc585/WHT_20150331/html/SDSSJ1339+1515/dimcombLR+bkgd_v138.ms.fits'
-#    wavelength, dw, flux, err = get_liris_spec(fname)
+    fname = '/data/lc585/WHT_20150331/html/SDSSJ1339+1515/dimcombLR+bkgd_v138.ms.fits'
+    wavelength, dw, flux, err = get_liris_spec(fname)
 #    wav, flux, err = rebin_spectra(wavelength,
 #                                   flux,
 #                                   er=err,
@@ -359,35 +369,36 @@ if __name__ == '__main__':
 
 
 
-    wav, dw, flux, err = get_boss_dr12_spec('SDSSJ133916.88+151507.6')
-
-    fit_line(wav,
-             flux,
-             err,
-             z=2.318977,
-             w0=np.mean([1548.202,1550.774])*u.AA,
-             velocity_shift=0.0*(u.km / u.s),
-             continuum_region=[[1445.,1465.]*u.AA,[1700.,1705.]*u.AA],
-             fitting_region=[1500.,1600.]*u.AA,
-             plot_region=[1440,1720]*u.AA,
-             nGaussians=0,
-             nLorentzians=1,
-             maskout=[[5107.3, 5138.2]]*u.AA,
-             verbose=True,
-             plot=True)
-
-#maskout=[[5107.3, 5138.2]]*u.AA,
-#maskout=[[-2064,-263]]*(u.km / u.s),
+#    wav, dw, flux, err = get_boss_dr12_spec('SDSSJ133916.88+151507.6')
 
 #    fit_line(wav,
 #             flux,
 #             err,
 #             z=2.318977,
-#             w0=6564.89*u.AA,
-#             maskout=[[4735.8, 4757.8]]*(u.AA),
-#             continuum_region=[[6000.,6250.]*u.AA,[6800.,7000.]*u.AA],
-#             plot_title='SDSSJ133916.88+151507.6',
-#             plot_region=[6000,7000]*u.AA)
+#             w0=np.mean([1548.202,1550.774])*u.AA,
+#             velocity_shift=0.0*(u.km / u.s),
+#             continuum_region=[[1445.,1465.]*u.AA,[1700.,1705.]*u.AA],
+#             fitting_region=[1500.,1600.]*u.AA,
+#             plot_region=[1440,1720]*u.AA,
+#             nGaussians=0,
+#             nLorentzians=1,
+#             maskout=[[5107.3, 5138.2]]*u.AA,
+#             verbose=True,
+#             plot=True)
+
+#maskout=[[5107.3, 5138.2]]*u.AA,
+#maskout=[[-2064,-263]]*(u.km / u.s),
+
+    fit_line(wavelength,
+             flux,
+             err,
+             z=2.318977,
+             w0=6564.89*u.AA,
+             maskout=[[4735.8, 4757.8]]*(u.AA),
+             continuum_region=[[6000.,6250.]*u.AA,[6800.,7000.]*u.AA],
+             plot_title='SDSSJ133916.88+151507.6',
+             plot_region=[6000,7000]*u.AA,
+             plot=True)
 
 
 
